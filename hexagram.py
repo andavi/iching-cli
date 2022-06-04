d777f060de6945cf37bad55f50ac5e243f4a6859
andavi <andrew.bradley.davis@gmail.com>
2022-06-04 08:04:31 -0700

    add explanation to eleborate Kennedy method of divination in comments

--- hexagram.py
+++ hexagram.py
@@ -9,38 +9,54 @@ table = [
 ]
 
 
-#######################################
-# Kennedy Method (see link)
-######################################
-# this is for the most nitpicky calculation of probablities for how
-# the yarrow stalks were thrown and interpretted according to the blurb 
-# by/[about Andrew Kennedy](https://en.wikipedia.org/wiki/I_Ching_divination#Dice)
-# according to his analysis the odds are 8/38 moving yang, 2/38 moving yin, 11/38 static yang,
-# and 17/38 static yin. These probabilities can be achieved using a 19-sided die and a coin which I 
-# simulate below:
-# | c | d  |  P    | line     |
-# |---|----|-------|----------|
-# |0  |<=2 |2/38   |mov yin   |
-# |0  | >2 |17/38  |stat yin  | 
-# |1  |<=8 |8/38   |mov yan   |
-# |1  | >8 |11/39  |stat yan  |
+def getLines():
+  #######################################
+  # Kennedy Method (see link)
+  ######################################
+  # this is for the most nitpicky calculation of probablities for how
+  # the yarrow stalks were thrown and interpretted according to the blurb 
+  # by/[about Andrew Kennedy](https://en.wikipedia.org/wiki/I_Ching_divination#Dice)
+  # according to his analysis the odds are 8/38 moving yang, 2/38 moving yin, 11/38 static yang,
+  # and 17/38 static yin. These probabilities can be achieved using a 19-sided die and a coin which I 
+  # simulate below:
+  # | c | d  |  P    | line     |
+  # |---|----|-------|----------|
+  # |0  |<=2 |2/38   |mov yin   |
+  # |0  | >2 |17/38  |stat yin  | 
+  # |1  |<=8 |8/38   |mov yan   |
+  # |1  | >8 |11/39  |stat yan  |
 
-# create coin toss and die toss function
-c = lambda: numIn(0,1)
-d = lambda: numIn(1,19)
+  # create coin toss and die toss function
+  c = lambda: numIn(0,1)
+  d = lambda: numIn(1,19)
 
-# fill len=6 array w/ coin and die toss values
-h = [[c(), d()] for _ in range(6)]
+  # fill len=6 array w/ coin and die toss values
+  h = [[c(), d()] for _ in range(6)]
 
-# tricky part: want to keep coin toss info so convert to string and 
-# concatenate rathher than add. then cast str to binary int. first bit
-# (coin toss) is larger in table: if c=0 then line will be table[0] or
-# table[1], so a binary zero needs to stay left in that case and v.v.
-hx = [int(str(l[0]) +  str(int(l[1] > 2 if l[0] == 0 else l[1] > 8)),2) for l in h]
+  # tricky part: want to keep coin toss info so convert to string and 
+  # concatenate rathher than add. then cast str to binary int. first bit
+  # (coin toss) is larger in table: if c=0 then line will be table[0] or
+  # table[1], so a binary zero needs to stay left in that case and v.v.
+  hx = [int(str(l[0]) +  str(int(l[1] > 2 if l[0] == 0 else l[1] > 8)),2) for l in h]
 
-# get lines that match rolls
-hexagram = [table[i] for i in hx]
+  # get lines that match rolls
+  return [table[i] for i in hx].append(hx.split())
+
+
+
+hexagram = getLines();
 
 # print from first line at botttom up as is traditional
 for i in range(5,-1,-1):
   print(hexagram[i])
+  
+  
+def movingLinesToOne(hexagram):
+  # As per Master Huang's instructions there should be only one or less moving lines.
+  # the original gua is reflecting your current situation, THe moving line then allows a line to alternative,
+  # pointing to an approached gua. An original and an approached gua with readings linking how to make
+  # the transition is the correct amount of info for a reading (or less ie no moving lines is fine).
+  # For this reason The Complete I Ching outlines an algorithm to get any readings w/ moving lines down to the single
+  # important moving line.. That is what this function does.
+  
+  
