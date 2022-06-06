3ecf5464397b8b9f99011be9fc72f5bb38aaa671
andavi <andrew.bradley.davis@gmail.com>
2022-06-05 22:22:23 -0700

    rule 2 of 7

--- hexagram.py
+++ hexagram.py
@@ -44,19 +44,39 @@ def getLines():
   return [table[i] for i in hx], hx
 
 
-gua, mx = getLines();
+# gua, mx = getLines();
 
-# print from first line at botttom up as is traditional
-for i in range(5,-1,-1):
-  print(gua[i], mx[i])
+# # print from first line at botttom up as is traditional
+# for i in range(5,-1,-1):
+#   print(gua[i], mx[i])
   
   
-def movingLinesToOne(hexagram):
+def movingLinesToOne(metahex):
   # As per Master Huang's instructions there should be only one or less moving lines.
   # the original gua is reflecting your current situation, THe moving line then allows a line to alternative,
   # pointing to an approached gua. An original and an approached gua with readings linking how to make
   # the transition is the correct amount of info for a reading (or less ie no moving lines is fine).
   # For this reason The Complete I Ching outlines an algorithm to get any readings w/ moving lines down to the single
   # important moving line.. That is what this function does.
-  metahex = hexagram[6]
+  umx = list(metahex)
   
+  # only looking for moving lines which are coded 0 and 2 in our table at top so search and count evens
+  movingLines = [n for n in umx if n % 2 == 0]
+  print(movingLines)
+  numMovingLines = len(movingLines)
+  
+  # if <= 1 then no work needs to be done - all good. return to sender
+  if numMovingLines <= 1:
+    return metahex
+  # if two moving lines and different types: consult yin only
+  if numMovingLines == 2 and len(set(movingLines)) == 2:
+    return [3 if l == 2 else l for l in umx ]
+  # if two moving lines the same => consult upper only
+  if numMovingLines == 2:
+    for i in range(6):
+      if umx[i] % 2 ==0:
+        umx[i] += 1
+        return umx
+  
+  
+print(movingLinesToOne([1,1,3,3,2,2]))
